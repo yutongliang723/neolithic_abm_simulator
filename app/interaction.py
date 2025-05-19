fe2ef3f9263ce89293e1c87f3351d3c288d2f7bf
yutongliang723 <susanliang723@gmail.com>
2025-05-20 01:04:29 +0200

    modified:   app/interaction.py

--- app/interaction.py
+++ app/interaction.py
@@ -190,7 +190,7 @@ def run_simulation_task(params):
     progress_percent = 100
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5002))
     app.run(debug=True, host='0.0.0.0', port=port)
 
 import os
